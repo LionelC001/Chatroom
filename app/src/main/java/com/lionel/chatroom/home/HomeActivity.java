38bb40cdfe56960d8707a176b77b39fba0abd58f
LionelC001 <lionelc15001@gmail.com>
2018-07-12 02:16:03 +0800

    Version: 22
    新增動態主畫面

--- app/src/main/java/com/lionel/chatroom/home/HomeActivity.java
+++ app/src/main/java/com/lionel/chatroom/home/HomeActivity.java
@@ -1,10 +1,11 @@
 package com.lionel.chatroom.home;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.lionel.chatroom.R;
 import com.lionel.chatroom.home.adapter.HomePagerAdapter;
@@ -15,8 +16,19 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         initViewPager();
+        initWallpaper();
+    }
+
+    private void initWallpaper() {
+        LinearLayout llLayoutHome = findViewById(R.id.ll_layout_home);
+        AnimationDrawable animWallpaper = (AnimationDrawable) getDrawable(R.drawable.anim_home_wallpaper);
+        llLayoutHome.setBackground(animWallpaper);
+        if (animWallpaper != null) {
+            animWallpaper.setEnterFadeDuration(2000);
+            animWallpaper.setExitFadeDuration(2000);
+            animWallpaper.start();
+        }
     }
 
     private void initViewPager() {
